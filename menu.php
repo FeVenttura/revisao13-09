8889bf95fc426c8a1cb34e32796f9eec285d10ba
FeVenttura <fe.vntr@gmail.com>
2024-09-13 22:05:55 -0300

    Revisao PHP + BD

---
+++ menu.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="container">
+        <nav class="menu-container">
+            <div class="logo">
+                <img src="assets/images/senac-logo.png" alt="">
+            </div>
+            <ul class="menu">
+                <li><a href="">Inicial</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
